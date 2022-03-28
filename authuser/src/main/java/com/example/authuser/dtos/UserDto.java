08899bb647b37a5e696d3ccab6ee66209b34ab30
Dilermando <dilermando10@gmail.com>
2022-03-28 02:16:01 -0300

    Spring Validation

--- authuser/src/main/java/com/example/authuser/dtos/UserDto.java
+++ authuser/src/main/java/com/example/authuser/dtos/UserDto.java
@@ -3,6 +3,10 @@ package com.example.authuser.dtos;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
@@ -19,15 +23,23 @@ public class UserDto {
 
     private UUID userId;
 
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Size(min=4, max=50)
     @JsonView(UserView.RegistrationPost.class)  //Username não poderá ser atualizado apenas inserido POST
     private String username;
 
+    @NotBlank
+    @Size(min=4, max=50)
+    @Email(groups = UserView.RegistrationPost.class)
     @JsonView(UserView.RegistrationPost.class)  // email não poderá ser atualizado Apenas inserido POST
     private String email;
 
+    @NotBlank(groups = {UserView.RegistrationPost.class,UserView.PasswordPut.class})
+    @Size(min=6, max=20)
     @JsonView({UserView.RegistrationPost.class,UserView.PasswordPut.class})  // password poderá ser inserido POST. e também Atualizado PUT
     private String password;
 
+    @NotBlank(groups = UserView.PasswordPut.class)
     @JsonView({UserView.PasswordPut.class})
     private String oldPassword;
 
@@ -40,6 +52,7 @@ public class UserDto {
     @JsonView({UserView.RegistrationPost.class,UserView.UserPut.class})
     private String cpf;
 
+    @NotBlank(groups = UserView.ImagePut.class)
     @JsonView({UserView.ImagePut.class})     // Só vai atualizar a imagem , não vai aparecer o "campo imagem"
     private String imageUrl;
 
